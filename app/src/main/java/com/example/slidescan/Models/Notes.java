0524b5dabe873c35de1a5f323f7778d8c77bf01d
Pratham <10015405@sbstudents.org>
2022-06-17 13:20:29 -0400

    formatting

--- app/src/main/java/com/example/slidescan/Models/Notes.java
+++ app/src/main/java/com/example/slidescan/Models/Notes.java
@@ -24,43 +24,33 @@ public class Notes implements Serializable
     @ColumnInfo(name = "pinned")
     Boolean pinned = false;
 
-    public int getId() {
-        return id;
-    }
+    public int getId() 
+    { return id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id)
+    { this.id = id; }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() 
+    { return title; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public void setTitle(String title) 
+    { this.title = title; }
 
-    public String getNotes() {
-        return notes;
-    }
+    public String getNotes() 
+    { return notes; }
 
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
+    public void setNotes(String notes) 
+    { this.notes = notes; }
 
-    public String getDate() {
-        return date;
-    }
+    public String getDate() 
+    { return date; }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+    public void setDate(String date) 
+    { this.date = date; }
 
-    public Boolean getPinned() {
-        return pinned;
-    }
+    public Boolean getPinned() 
+    { return pinned; }
 
-    public void setPinned(Boolean pinned) {
-        this.pinned = pinned;
-    }
+    public void setPinned(Boolean pinned) 
+    { this.pinned = pinned; }
 }
